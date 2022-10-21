922f96502b0903be7f46897f287e4118494c6cb4
Andrey <Sm1le84@mail.ru>
2022-10-21 20:28:13 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,17 @@
+public class BonusMilesService {
+
+     public int calculate (int cost) {
+         int bonusMilesValue = cost / 20;
+
+         return bonusMilesValue;
+
+
+
+
+
+    }
+
+
+
+
+}
